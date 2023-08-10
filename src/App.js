cef4123870abd21f91e611339578c9f2867882b7
diksha2150487 <130853102+diksha2150487@users.noreply.github.com>
2023-08-10 18:57:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,371 @@
+import "./App.css";
+import img1 from "./images/bigcommerce-bg.png.png";
+import img2 from "./images/Book-a-free-consultation 1 (1).png";
+import img3 from "./images/SVG.png";
+import img4 from "./images/SVG (1).png";
+import img5 from "./images/SVG (2).png";
+import img6 from "./images/SVG (3).png";
+import img7 from "./images/SVG (4).png";
+import img8 from "./images/SVG (6).png";
+import img9 from "./images/SVG (7).png";
+import img10 from "./images/SVG (8).png";
+import img11 from "./images/SVG (9).png";
+import img12 from "./images/SVG (10).png";
+import img13 from "./images/SVG (11).png";
+import img14 from "./images/SVG (12).png";
+import img15 from "./images/SVG (13).png";
+import img16 from "./images/SVG (14).png";
+import img17 from "./images/SVG (15).png";
+import img18 from "./images/Figure → Link → TRAVEL-HOSPITALITY.jpg.png";
+import img19 from "./images/Figure → Link → LOGISTIC.jpg.png";
+import img20 from "./images/Figure → Link → accouting.jpg.png";
+import img21 from "./images/Figure → Link → CONSTRUCTION.jpg.png";
+import img22 from "./images/Figure → Link → BANKING-FINANCE.jpg.png";
+import img23 from "./images/Figure → Link → MEDIA-ADVT.jpg.png";
+import img24 from "./images/Figure → Link → HEALTH-CARE.jpg.png";
+import img25 from "./images/Figure → Link → ECOMMERCE.jpg.png";
+import img26 from "./images/Figure → Link → FOOD.jpg.png";
+import img27 from "./images/Picture → lets-connect.jpg.webp.png";
+// import img28 from "./images/div.service-list-num.png";
+// import img29 from "./images/div.service-list-num (5).png";
+// import img30 from "./images/div.service-list-num (2).png";
+// import img31 from "./images/div.service-list-num (3).png";
+// import img32 from "./images/div.service-list-num (4).png";
+// import img33 from "./images/div.service-list-num (1).png";
+
+function App() {
+  return (
+    <div>
+      <div className="paper1">
+        <div className="leftpaper1">
+          <p className="Ambitious">BigCommerce Development Service</p>
+          <p className="brand">
+            Eye catching and inviting bigcommerce storefronts that wow your
+            customers and keep them coming back for more. From BigCommerce
+            Website Development and Integration to BigCommerce Theme Development
+            and Design, we have successfully done it all!
+          </p>
+          <button className="team">LET’S TALK</button>
+        </div>
+        <div className="rightpaper1">
+          <img src={img1} alt="img" className="three-circle" />
+        </div>
+      </div>
+      {/* ******************************************************************************** */}
+      <div className="our-side">
+        <div className="host-left">
+          <p className="hills">
+            Grow Big with <span className="koyal">BigCommerce</span> Development
+            Services.
+          </p>
+          <p className="mangeto">
+            Ever heard of the phrase “dream big”? What if we tell you that we
+            can make that dream come true? Big Commerce is known to be big on
+            scalability and help you grow your business at double the current
+            industry average rate (28% per annum compared to the industry
+            average of 14%). Kushel Digi is a leading Big Commerce agency and
+            Big Commerce development company, and our expert big commerce
+            developers can enable you to grow bigger and better while never
+            letting you compromise on the quality of your store. Your ecommerce
+            website development with Big Commerce will be one of the best
+            decisions you make with its self-explanatory admin panel,
+            user-friendly interface, rapid deployment, round-the-clock service
+            and support, customizable design and powerful functionality.
+          </p>
+          <button className="console">Book a Free Consultation</button>
+        </div>
+        <div className="host-right">
+          <img src={img2} className="network" alt="logo"></img>
+        </div>
+      </div>
+      {/* ************************************************************* */}
+      <div className="teach-team">
+        <p className="glory">
+          Why <span className="koyal">BigCommerce </span>is the best for your
+          Business
+        </p>
+        <p className="strick">
+          Whether you are a startup, or an established enterprise, we can use
+          the BigCommerce platform to build a cutting-edge, differentiated
+          solution for you without you having to make any compromises. Here’s
+          why Folio3 BigCommerce Development Company may be the best choice for
+          you.
+        </p>
+        <div className="card-various">
+          <div className="dogers-first">
+            <div className="first1">
+              <img src={img3} className="watch-time" />
+              <h4>Cost Efficient</h4>
+              <h5>
+                Cloud-based solution ensures only a quarter of the cost is
+                incurred compared to on- premise ecommerce solutions
+              </h5>
+            </div>
+            <div className="first1">
+              <img src={img4} className="watch-time" />
+              <h4>Unparalleled Uptime</h4>
+              <p className="global-store">
+                Best up-time in the industry at 99.99%, even leading up to 100%
+                on the busiest ecommerce sale days, like Cyber Week
+              </p>
+            </div>
+            <div className="first1">
+              <img src={img5} className="watch-time" />
+              <h4>Rapid Response Time</h4>
+              <p className="global-store">
+                Lightning fast connection time of under 10ms leading to ultra
+                fast page loading and anoutstanding customer experience
+              </p>
+            </div>
+          </div>
+          <div className="dogers-second">
+            <div className="first1">
+              <img src={img6} className="watch-time" />
+              <h4>Reduced Deployment Time</h4>
+              <h5>
+                Cloud-based solution ensures only half the time is spent in
+                deployment compared to on- premise ecommerce solutions
+              </h5>
+            </div>
+            <div className="first1">
+              <img src={img7} className="watch-time" />
+
+              <h4>Exceptional Shopping Experience</h4>
+              <h5>
+                One page checkout, faceted search and navigation, dynamic
+                merchandising, instantaneous shipping quotes, auto-saved...
+              </h5>
+            </div>
+            <div className="first1">
+              <img src={img8} className="watch-time" />
+
+              <h4>Optimized for Mobile</h4>
+              <h5>
+                BigCommerce ERP integration Apps for third- party softwares
+              </h5>
+            </div>
+          </div>
+          <div className="dogers-third">
+            <div className="first1">
+              <img src={img9} className="watch-time" />
+              <h4>Completely Customizable</h4>
+              <h5>
+                BigCommerce ERP integration Apps for third- party softwares
+              </h5>
+            </div>
+            <div className="first1">
+              <img src={img10} className="watch-time" />
+
+              <h4>Highly Secure</h4>
+              <h5>
+                PCI Compliance, DDOS mitigation when needed, and HTTPS site-wide
+              </h5>
+            </div>
+            <div className="first1">
+              <img src={img11} className="watch-time" />
+              <h4>Scalable</h4>
+              <h5>
+                BigCommerce ERP integration Apps for third- party softwares
+              </h5>
+            </div>
+          </div>
+          <div className="dogers-fourth">
+            <div className="first1">
+              <img src={img12} className="watch-time" />
+              <h4>Cross-Channel Integration</h4>
+              <h5>
+                Integration with all the top marketplaces like Amazon, Ebay, and
+                Google Shopping, and social media platforms like Facebook,
+                Instagram and Pinterest.
+              </h5>
+            </div>
+            <div className="first1">
+              <img src={img13} className="watch-time" />
+              <h4>Outstanding SEO Features</h4>
+              <h5>
+                Out-of-the-box Meta Descriptions, robots.exe editing, image
+                optimization, and customizable SEO compliant URLs get you more
+                traffic
+              </h5>
+            </div>
+            <div className="first1">
+              <img src={img14} className="watch-time" />
+              <h4>POS Integration</h4>
+              <h5>
+                Integration with all the top marketplaces like Amazon, Ebay, and
+                Google Shopping, and social media platforms like Facebook,
+                Instagram and Pinterest.
+              </h5>
+            </div>
+          </div>
+          <div className="dogers-five">
+            <div className="first1">
+              <img src={img15} className="watch-time" />
+              <h4>Extensive Integration Support</h4>
+              <h5>
+                Easy integration with Payment Gateways, Accounting Solutions,
+                Shipping Systems, ERPs, Tax Systems, POS Systems, Marketing
+                Automation Tools, etc
+              </h5>
+            </div>
+            <div className="first1">
+              <img src={img16} className="watch-time" />
+              <h4>APIs</h4>
+              <h5>
+                Huge breadth (over 90%) of powerful APIs makes BigCommerce an
+                extremely flexible ecommerce platform which opens up multiple
+                integration possibilities
+              </h5>
+            </div>
+            <div className="first1">
+              <img src={img17} className="watch-time" />
+              <h4>Restore & Backup</h4>
+              <h5>
+                Backup your ecommerce store data with two data centers and an
+                additional third spatially separated data center to fall back on
+                in case of data loss.
+              </h5>
+            </div>
+          </div>
+        </div>
+        <button className="naps">TALK TO OUR DEVELOPER TODAY</button>
+      </div>
+      {/* *************************************************************************** */}
+      {/* <div className="dogger_cards">
+        <p>BigCommerce Development Services We Offer</p>
+        <div className="aligned_card">
+          <div className="card_withicon">
+            <img src={img28} alt="img" />
+            <p className="cardHeader">Design & Develop Your Store with us</p>
+            <p className="cardContent">
+              Bigcommerce Development has a great collection of pre-loaded
+              themes. It is essential to select the best Bigcommerce Development
+              Company as they will reflect your brand identity. Our team will
+              work on creating and customizing the website to stand out in the
+              crowd.
+            </p>
+          </div>
+          <div className="card_withicon">
+            <img src={img29} alt="img" />
+            <p className="cardHeader">
+              Integrate & Customize Your BigCommerce Theme!
+            </p>
+            <p className="cardContent">
+              Your design and look will make the first impression on your
+              customers. With the help of the Bigcommerce Development agency,
+              your e-commerce store will breathe a new life in the industry,
+              quickly leveraging the customers.
+            </p>
+          </div>
+          <div className="card_withicon">
+            <img src={img30} alt="img" />
+            <p className="cardHeader">Make Your Platform SEO-Friendly!</p>
+            <p className="cardContent">
+              Stepping into the online world, you know how important it is to
+              handle SEO to reach the search engines’ top rankings.Fortunately,
+              you have us, the Bigcommerce E-commerce development experts, to
+              take your stress away. We are not limited to using the best SEO
+              tools but will analyze the growth regularly and maintain your
+              website’s top ranks.
+            </p>
+          </div>
+          <div className="card_withicon">
+            <img src={img31} alt="img" />
+            <p className="cardHeader">
+              Are You Finding Difficulty in API Integration?
+            </p>
+            <p className="cardContent">
+              There should be coordination between data, devices, and
+              applications to run a successful e- commerce store. We will handle
+              the Bigcommerce web development by providing third-party API
+              integration and managing the module. Our team will keep your
+              requirements and expectations in mind all the time to make the
+              website accordingly.
+            </p>
+          </div>
+          <div className="card_withicon">
+            <img src={img32} alt="img" />
+            <p className="cardHeader">
+              Are You Finding Difficulty in API Integration?
+            </p>
+            <p className="cardContent">
+              There should be coordination between data, devices, and
+              applications to run a successful e- commerce store. We will handle
+              the Bigcommerce web development by providing third-party API
+              integration and managing the module. Our team will keep your
+              requirements and expectations in mind all the time to make the
+              website accordingly.
+            </p>
+          </div>
+          <div className="card_withicon">
+            <img src={img33} alt="img" />
+            <p className="cardHeader">
+              Are You Finding Difficulty in API Integration?
+            </p>
+            <p className="cardContent">
+              There should be coordination between data, devices, and
+              applications to run a successful e- commerce store. We will handle
+              the Bigcommerce web development by providing third-party API
+              integration and managing the module. Our team will keep your
+              requirements and expectations in mind all the time to make the
+              website accordingly.
+            </p>
+          </div>
+        </div>
+      </div> */}
+      {/* ************************************************************************************* */}
+      <div className="Iot-shape">
+        <p className="lofty">Industries We Have Served</p>
+        <p className="otals">
+          Our BigCommerce Development Service range consists of both domestic
+          and international clientele. Over the years, we have a prolific
+          history of working with some of the high-profile tech-driven
+          industries of all sizes and categories worldwide.
+        </p>
+        <div className="images-main-box">
+          <div className="canny-box">
+            <img src={img18} className="tigers"></img>
+            <img src={img18} className="tigers"></img>
+          </div>
+          <div className="canny-three-box">
+            <img src={img19} className="accountion"></img>
+            <img src={img20} className="accountion"></img>
+            <img src={img21} className="accountion"></img>
+          </div>
+          <div className="joly-box">
+            <img src={img22} className="tigers"></img>
+            <img src={img23} className="tigers"></img>
+          </div>
+          <div className="canny-three-box">
+            <img src={img24} className="accountion"></img>
+            <img src={img25} className="accountion"></img>
+            <img src={img26} className="accountion"></img>
+          </div>
+        </div>
+      </div>
+      {/* ***************************************************************************** */}
+      <div className="box-protifilo">
+        <p className="recoil">
+          Our <span className="koyal">BigCommerce </span>Web Development
+          <span className="koyal"> Portfolio</span>
+        </p>
+      </div>
+      {/* ***************************************************************************** */}
+      <div className="boeing-men">
+        <div className="Eurpoe-left">
+          <img src={img27} className="two-women"></img>
+        </div>
+        <div className="Eurpoe-right">
+          <div className="topice">
+            <p className="fortly">Run your </p>
+            <p className="fortly">business efficiently</p>
+            <p className="fortly">with Magento</p>
+          </div>
+          <p className="apprel">Let’s Connect</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
